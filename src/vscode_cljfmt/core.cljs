96319b702ff53368a2af34b4f6d010a8ed1a6935
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-05-26 08:17:47 +0700

    Parse configuration per call

--- src/vscode_cljfmt/core.cljs
+++ src/vscode_cljfmt/core.cljs
@@ -16,15 +16,17 @@
 (deftype ClojureDocumentRangeFormattingEditProvider [m]
   Object
   (provideDocumentRangeFormattingEdits [_ document range options token]
-    (let [{:keys [configuration catch*]} m
-          
-          text    (.getText document range)
+    (let [{:keys [catch*]} m
 
-          pretty  (try
-                    (cljfmt/reformat-string text configuration)
-                    (catch js/Error e
-                      (catch* e)
-                      nil))]
+          configuration (parse-configuration (vscode/workspace.getConfiguration "cljfmt"))
+
+          text          (.getText document range)
+
+          pretty        (try
+                          (cljfmt/reformat-string text configuration)
+                          (catch js/Error e
+                            (catch* e)
+                            nil))]
 
       (when pretty
         #js [(vscode/TextEdit.replace range pretty)]))))
@@ -33,10 +35,8 @@
 (defn activate [^js context]
   (let [scheme        #js {:language "clojure" :scheme "file"}
         output        (vscode/window.createOutputChannel "cljfmt")
-        configuration (vscode/workspace.getConfiguration "cljfmt")
-        provider      (ClojureDocumentRangeFormattingEditProvider. {:configuration (parse-configuration configuration)
-                                                                    :catch*        (fn [e]
-                                                                                     (.appendLine output (.-message e)))})]
+        provider      (ClojureDocumentRangeFormattingEditProvider. {:catch* (fn [e]
+                                                                              (.appendLine output (.-message e)))})]
 
     (.push context.subscriptions (vscode/Disposable.from output))
 
