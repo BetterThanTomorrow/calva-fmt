0c8b7ddbf3a89b9ac92096d3771cfaea8634c2ae
Pedro Girardi <pedro.rogerio.girardi@gmail.com>
2018-06-07 15:09:25 +0700

    Select all clojure documents - even those that are not saved on disk

--- src/calva/fmt/core.cljs
+++ src/calva/fmt/core.cljs
@@ -4,6 +4,5 @@
    [calva.fmt.range-formatting-provider :as range-formatting]))
 
 (defn activate [^js context]
-  (let [scheme        #js {:language "clojure" :scheme "file"}
-        provider      (range-formatting/RangeEditProvider.)]
-    (.push context.subscriptions (vscode/languages.registerDocumentRangeFormattingEditProvider scheme provider))))
+  (let [provider (range-formatting/RangeEditProvider.)]
+    (.push context.subscriptions (vscode/languages.registerDocumentRangeFormattingEditProvider "clojure" provider))))
