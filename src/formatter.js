63f66e777bf01afe386bf89b5d994ae7c10a146e
Peter Strömberg <pez@pezius.com>
2018-06-19 14:15:38 +0200

    Factored out formatting code so the provider id as thin as it can be.

---
+++ src/formatter.js
@@ -0,0 +1,36 @@
+const vscode = require('vscode');
+const { formatText, cljify, jsify } = require('../cljc_out/calva_formatter');
+
+function readConfiguration() {
+    let workspaceConfig = vscode.workspace.getConfiguration("calva.fmt")
+    return {
+        "indentation?": workspaceConfig.get("indentation"),
+        "remove-surrounding-whitespace?": workspaceConfig.get("removeSurroundingWhitespace"),
+        "remove-trailing-whitespace?": workspaceConfig.get("removeTrailingWhitespace"),
+        "insert-missin-whitespace?": workspaceConfig.get("insertMissingWhitespace")
+    };
+}
+
+function getConfig() {
+    let config = readConfiguration();
+    config["remove-consecutive-blank-lines?"] = false; // Until we understand its behaviour better
+    return config;
+}
+
+function format(text) {
+    config = getConfig();
+    let d = { "text": text, "config": config };
+    d = formatText(cljify(d));
+    d = jsify(d);
+    if (!d["error"]) {
+        return d["new-text"];
+    }
+    else {
+        console.log(d["error"]);
+        return text;
+    }
+}
+
+module.exports = {
+    format
+}
--- src/providers/range_formatter.js
+++ src/providers/range_formatter.js
@@ -1,31 +1,11 @@
 const vscode = require('vscode');
-const { formatText, cljify, jsify } = require('../../cljc_out/calva_formatter');
-
-function readConfiguration(configuration) {
-    return {
-        "indentation?": configuration.get("indentation"),
-        "remove-surrounding-whitespace?": configuration.get("removeSurroundingWhitespace"),
-        "remove-trailing-whitespace?": configuration.get("removeTrailingWhitespace"),
-        "insert-missin-whitespace?": configuration.get("insertMissingWhitespace")
-    };
-}
+const formatter = require('../formatter');
 
 class RangeEditProvider {
     provideDocumentRangeFormattingEdits(document, range, options, token) {
-        let config = readConfiguration(vscode.workspace.getConfiguration("calva.fmt")),
-            text = document.getText(range);
-
-        config["remove-consecutive-blank-lines?"] = false;
-        let dict = { "text": text, "config": config };
-        let newDict = formatText(cljify(dict));
-        newDict = jsify(newDict);
-        if (!newDict["error"]) {
-            return [vscode.TextEdit.replace(range, newDict["new-text"])];
-        }
-        else {
-            console.log(newDict["error"]);
-            return [];
-        }
+        let text = document.getText(range),
+            newText = formatter.format(text);
+        return [vscode.TextEdit.replace(range, newText)];
     }
 }
 
