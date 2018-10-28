c2f952836d7452fe6b61f88479693c85e550dedf
Peter Strömberg <pez@pezius.com>
2018-10-28 21:11:22 +0100

    Format on newlines

--- calva-fmt/config.ts
+++ calva-fmt/config.ts
@@ -7,12 +7,12 @@ function readConfiguration() {
         "indentation?": workspaceConfig.get("indentation"),
         "remove-surrounding-whitespace?": workspaceConfig.get("removeSurroundingWhitespace"),
         "remove-trailing-whitespace?": workspaceConfig.get("removeTrailingWhitespace"),
-        "insert-missin-whitespace?": workspaceConfig.get("insertMissingWhitespace")
+        "insert-missing-whitespace?": workspaceConfig.get("insertMissingWhitespace"),
+        "remove-consecutive-blank-lines?": workspaceConfig.get("removeConsecutiveBlankLines")
     };
 }
 
 export function getConfig() {
     let config = readConfiguration();
-    config["remove-consecutive-blank-lines?"] = false; // Until we understand its behaviour better
     return config;
 }
--- calva-fmt/providers/ontype_formatter.ts
+++ calva-fmt/providers/ontype_formatter.ts
@@ -1,15 +1,16 @@
 import * as vscode from 'vscode';
 import * as config from '../config';
-const { indentForIndex, cljify, jsify } = require('../../lib/calva_fmt');
+import * as formatter from '../format';
+const { formatTextAtIdx, cljify, jsify } = require('../../lib/calva_fmt');
 
 
 function calculateIndent(document: vscode.TextDocument, pos: vscode.Position): number {
     let allText: string = document.getText(),
         idx: number = document.offsetAt(pos),
         data: object = { "all-text": allText, "idx": idx, "config": config.getConfig() },
-        result: object = jsify(indentForIndex(cljify(data)));
+        result: object = jsify(formatTextAtIdx(cljify(data)));
     if (!result["error"]) {
-        return result["indent"];
+        return result["new-index"];
     } else {
         console.log(result["error"]);
         return pos.character;
@@ -17,7 +18,31 @@ function calculateIndent(document: vscode.TextDocument, pos: vscode.Position): n
 }
 
 
-export class OnTypeEditProvider {
+export class FormaOnTypeEditProvider {
+    provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, _ch, _options) {
+        formatter.formatPositionCommand(vscode.window.activeTextEditor);
+        return null;
+    }
+    //     if (config.getConfig()["adjust-cursor-position?"]) {
+    //         let indent = calculateIndent(document, position),
+    //             startPosition = position.with(position.line, 0);
+    //         if (position.character !== undefined) {
+    //             if (position.character > indent) {
+    //                 return [vscode.TextEdit.delete(new vscode.Range(position.with(position.line, indent), position))];
+    //             } else {
+    //                 return [vscode.TextEdit.insert(startPosition, ' '.repeat(indent - position.character))];
+    //             }
+    //         } else {
+    //             return null;
+    //         }
+    //     } else {
+    //         return null;
+    //     }
+    // }
+}
+
+
+export class OnlyAdjustIndentOnTypeEditProvider {
     provideOnTypeFormattingEdits(document: vscode.TextDocument, position: vscode.Position, _ch, _options) {
         if (config.getConfig()["adjust-cursor-position?"]) {
             let indent = calculateIndent(document, position),
